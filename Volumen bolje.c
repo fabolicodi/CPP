5af62076e6d469c56b0b0c7edd34520adbb31d9d
fabolicodi <95843185+fabolicodi@users.noreply.github.com>
2021-12-09 08:38:40 +0100

    Add files via upload

---
+++ Volumen bolje.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main() {
+int stranicaA=8;
+int stranicaB=5;
+int stranicaC=4;
+printf("Unesi stranicu\n");
+scanf ("%d %d %d",&stranicaA,&stranicaB,&stranicaC);
+int volumen=stranicaA*stranicaB*stranicaC;
+printf("%d",volumen);
+}
