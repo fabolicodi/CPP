7e8cb3d58d698152e75581fe333bfb892a42a963
fabolicodi <95843185+fabolicodi@users.noreply.github.com>
2021-12-16 09:31:29 +0100

    Add files via upload

---
+++ Troukt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int a;
+    int b;
+
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    int p=(a*b)/2;
+
+    printf("Povrsina pravokutnkog trokuta = %d\n",p);
+    return 0;
+}
