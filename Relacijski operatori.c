53d1eabb8465741c959b66ed051e6e1184e82a07
fabolicodi <95843185+fabolicodi@users.noreply.github.com>
2021-12-16 10:05:36 +0100

    Add files via upload

---
+++ Relacijski operatori.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+printf("4 > 6 -> %d.\n",(4>6));
+printf("4 < 6 -> %d.\n", (4<6));
+printf("4 >= 6 -> %d.\n", (4>=6));
+printf("4 <= 6 -> %d.\n", (4<=6));
+printf("4 != 6 -> %d.\n", (4!=6));
+printf("4 == 6 -> %d.\n", (4==6));
+
+
+return 0;
+
+
+
+
+
+}
