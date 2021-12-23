1a8fea3f0e42378c6230ff8343ea80b05da2b5a7
fabolicodi <95843185+fabolicodi@users.noreply.github.com>
2021-12-23 08:41:58 +0100

    Add files via upload

---
+++ Razlomci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+
+int a;
+int b;
+int c;
+int d;
+printf("Unesi duljinu\n");
+scanf("%d %d %d %d",&a,&b,&c,&d);
+int y=b==d?b:b*d;
+int x=b==d?a+c:a*d+c*b;
+
+printf("rezultat = %d / %d\n",x,y);
+printf("rezultat =   %.2f\n",(float)x/y);
+
+
+
+return 0;
+}
