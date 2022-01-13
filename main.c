e5616ed7f71b7701e89f7b5e7231035bffbd9b55
fabolicodi <95843185+fabolicodi@users.noreply.github.com>
2022-01-13 10:41:11 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+
+int c;
+int h;
+int m;
+printf ("Unesi cijenu sate i minute\n");
+scanf("%d %d %d",&c,&h,&m);
+ int a = h+m;
+ int r = c-a;
+ printf("Cijena je = %d kn",r);
+ return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
