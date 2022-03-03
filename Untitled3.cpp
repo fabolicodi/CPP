3874e73bd500555f34f50c3a1e4803ea55d6d3be
fabolicodi <95843185+fabolicodi@users.noreply.github.com>
2022-03-03 10:37:34 +0100

    Add files via upload

---
+++ Untitled3.cpp
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main (){
+int a,b,c,d;
+
+printf(" -------------------------------\n");
+printf("| \t\t\t\t|\n");
+printf("| \t Unesi danasnji\t\t|\n");
+printf("| \t datum i broj koji \t|\n");
+printf("| \t je dan u tjednu\t|\n");
+printf("| \t\t\t\t|\n");	
+printf(" -------------------------------\n");
+scanf(" %d %d %d %d",&a,&b,&c,%d);
+if (1<=a<=31);
+prtinf("%d.",a);
+switch (b){
+	case 1:
+		printf("Sijecanj");
+	case 2:
+		printf("Veljaca");
+	case 3:
+		printf("Ozujak");
+	case 4:
+		printf("Travanj");
+	case 5:
+		printf("Svibanj");
+	case 6:
+		printf("Lipanj"):
+	case 7:
+		printf("Srpanj");
+	case 8:
+		printf("Kolovoz");
+	case 9:
+		printf("Rujan");
+	case 10:
+		printf("Listopad");
+	case 11:
+		printf("Studeni");
+	case 12:
+		printf("Prosinac");
+}
+if (2000<=c<=2050)
+printf("%d."c);
+
+switch (d){
+	case 1:
+		printf("ponedjelaj");
+	case 2:
+		printf("Veljaca");
+	case 3:
+		printf("Ozujak");
+	case 4:
+		printf("Travanj");
+	case 5:
+		printf("Svibanj");
+	case 6:
+		printf("Lipanj"):
+	case 7:
+		printf("Srpanj");
+	case 8:
+		printf("Kolovoz");
+}
