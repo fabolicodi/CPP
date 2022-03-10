85d24deee8c6ba4de043b951e354e70cce6c8d70
fabolicodi <95843185+fabolicodi@users.noreply.github.com>
2022-03-10 07:36:38 +0100

    Add files via upload

---
+++ Untitled5.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main (){
+	
+int a;
+printf("Unesi broj\n");
+scanf("%d",&a);
+if (a%2==0)
+printf("Broj je paran\n");
+
+if (a%2==1)
+printf("Broj je neparan\n");
+
+	
+return 0;
+	
+
+}
