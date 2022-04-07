fb1d5380a4ddd37f272cd800a293baba7f62f125
fabolicodi <95843185+fabolicodi@users.noreply.github.com>
2022-04-07 08:01:16 +0200

    Add files via upload

---
+++ RORI AUTO TAJ.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+	
+float a,b,c,d=1;
+while (d){
+a=b;
+b=c;
+c=d;
+scanf("%f",&d);
+}
+printf("razlika %.2f \n",a-c);
+
+}
