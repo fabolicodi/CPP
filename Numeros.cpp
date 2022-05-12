112e87b8fa1e9fb723c8e1efd90251b7388c0cef
fabolicodi <95843185+fabolicodi@users.noreply.github.com>
2022-05-12 08:25:08 +0200

    Add files via upload

---
+++ Numeros.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+int i;
+int x[6];
+for(i=0;i<6;i++){  //upis podataka
+	printf("Unesi Broj \n");
+	scanf("%d",&x[i]);
+}	
+
+printf("\n");
+for(i=0;i<6;i++) {   //ispis podataka
+	printf("%d \n",x[i]);
+}
+return 0;	
+}
